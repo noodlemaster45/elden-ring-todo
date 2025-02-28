9da2be657a267caa77c278f7ed63fd809a5bea82
noodlemaster45 <nguyenquocdat12a7@gmail.com>
2025-02-28 13:52:32 +0700

    add update function

--- src/components/Boss.vue
+++ src/components/Boss.vue
@@ -1,12 +1,22 @@
 <script setup>
 import card from "@/components/Card.vue"
 import { defineProps, ref } from "vue";
+import axios from "axios";
 const props = defineProps({
     boss: Object
 });   
-const bossData = ref({})
-const setBeaten = () =>{
-    bossData.beaten = props.boss;
+const scrollY = ref(0);
+const setBeaten = async (boss) =>{
+try {
+    boss.beaten = true;
+    await axios.patch(`http://localhost:3000/bosses/${boss.id}`,boss),{
+        headers: { "Content-Type": "application/json" }
+    };
+    nextTick(() => window.scrollTo(0, scrollY.value));
+    console.log('enemy felled')
+} catch (error) {
+    console.error('error');
+}
 }
 </script>
 <template>
@@ -15,7 +25,7 @@ const setBeaten = () =>{
         <p class="mt-2 mb-4">
             {{ boss.location }}
         </p>
-        <a v-if="!boss.beaten" v-on:click="setBeaten" class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700 button">
+        <a v-if="!boss.beaten" v-on:click.prevent="setBeaten(boss)" class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700 button">
             Felled
         </a>
     </card>
--- src/components/Grids.vue
+++ src/components/Grids.vue
@@ -4,6 +4,18 @@ import Boss from "@/components/Boss.vue"
 import { ref, onMounted } from "vue";
 import axios from "axios";
 const bosses = ref([]);
+const resetBeaten = async () =>{
+try {
+    const resetBosses = bosses.value.map(boss => ({ ...boss, beaten: false }));
+    await Promise.all(resetBosses.map(boss =>
+      axios.put(`http://localhost:3000/bosses/${boss.id}`, boss)
+    ));
+    bosses.value = resetBosses;
+    console.log("All bosses reset to unbeaten.");
+} catch (error) {
+    console.error("Error resetting bosses:", error);
+}
+}
 onMounted(async()=>{
     try {
         const response = await axios.get('http://localhost:3000/bosses');
@@ -11,9 +23,13 @@ onMounted(async()=>{
     } catch (error) {
         console.error('you dumb dumb');
     }
-})
+}
+)
 </script>
 <template>
+    <a  v-on:click.prevent="resetBeaten(boss)" class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700 button">
+            Reset
+        </a>
     <div class="grid grid-cols-3 gap-5 bg-gray-100">
         <Boss v-for="boss in bosses" :key="boss.id" :boss="boss">
         </Boss>
